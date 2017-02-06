8e3e7772eee612524fd61f7beebf15829f2ff105
Steve Mills <stevepatrickmills@gmail.com>
2017-02-06 17:33:53 -0600

    Add Query Function for URL

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
-//GET /todos?completed=true
+//GET /todos?completed=false&q=work
 app.get('/todos', function (req, res) {
 	var queryParams = req.query;
 	var filteredTodos = todos;
@@ -24,6 +24,14 @@ app.get('/todos', function (req, res) {
  } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
  	filteredTodos = _.where(filteredTodos, {completed: false});
  }
+
+if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+		filteredTodos = _.filter(filteredTodos, function (todo) {
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1; 
+		});
+	
+ 	 }
+
 	res.json(filteredTodos);
 });
 
@@ -103,10 +111,6 @@ app.put('/todos/:id', function (req, res) {
 
 
 });
-
-
-
-
 
 app.listen(PORT, function () {
 	console.log('Exoress listing on port: ' + PORT + '!');
