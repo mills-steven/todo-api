d6692010c3796eba25ef1f13ef555ce614516e7d
Steve Mills <stevepatrickmills@gmail.com>
2017-02-06 16:46:08 -0600

    Adding variable-playground.js

---
+++ variable-playground.js
@@ -0,0 +1,27 @@
+/*var person = {
+	 name: 'Steve',
+	 age: '30'
+}
+
+function updatePerson (obj) {
+	//obj = { 
+	//	name: 'Romana',
+	//	age: '35'
+	//}
+	obj.age	= '32'
+}
+updatePerson(person);
+console.log(person);
+*/
+
+//Array Example
+
+var grades = [13,36]
+
+function updateGrades (grade) {
+	grade.push(26);
+	debugger;
+}
+
+updateGrades(grades);
+console.log(grades);
