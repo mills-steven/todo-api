f8b92e4fb8f88f342ff732efea96aa95e0578dbf
Steve Mills <stevepatrickmills@gmail.com>
2017-02-16 19:18:46 -0600

    Convert GET todo ID to use sequelize

--- server.js
+++ server.js
@@ -45,42 +45,32 @@ app.get('/todos', function(req, res) {
 //GET /todos/:id
 app.get('/todos/:id', function(req, res) {
 	var todoID = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoID
+	
+	db.todo.findById(todoID).then(function (todo){
+		if (!! todo) {
+			res.json(todo.toJSON());
+		} else {
+			res.status(404).send();
+		}
+	
+	}, function (e) {
+		res.status(500).send();
 	});
-
-
-	if (matchedTodo) {
-		res.json(matchedTodo);
-	} else {
-		res.status(404).send();
-	}
 });
 
 
 // POST Request 
 
 app.post('/todos', function(req, res) {
-	//var body = req.body;
 	var accpetedBody = _.pick(req.body, 'description', 'completed');
 
 	db.todo.create(accpetedBody).then(function (todo) {
 		res.json(todo.toJSON());
 
-
 	}, function(e) {
 		res.status(400).json(e);
 	});
 
-	/*if (!_.isBoolean(accpetedBody.completed) || !_.isString(accpetedBody.description) || accpetedBody.description.trim().length === 0) {
-
-		return res.status(400).send();
-	}
-
-	accpetedBody.description = accpetedBody.description.trim();
-	accpetedBody.id = todoNextId++;
-	todos.push(accpetedBody);
-	res.json(accpetedBody);*/
 });
 
 
